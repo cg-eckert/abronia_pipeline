66219be5667d0c7ac25b0a6b4958c4c6f671db1d
Chris Eckert <chris.eckert@queensu.ca>
2013-06-22 14:19:41 -0300

    Update read_mapping.sh

--- read_mapping.sh
+++ read_mapping.sh
@@ -2,3 +2,5 @@
 
 cd /storage/chris.eckert/mapping
 
+bowtie -S -p 2 selfer_built -1 ../trimmed_reads/CCC_R1_trimmed_clipped_stillpaired.fastq -2 ../trimmed_reads/CCC_R2_trimmed_clipped_stillpaired.fastq CCC.sam
+
